2963c89941a3507870314b72117e45c74eee6f92
Rebecca Peckman <rebeccapeckman@gmail.com>
2022-05-10 17:38:06 -0700

    knex configs refactored, db reset, knex scripts work

---
+++ data/db-config.ts
@@ -0,0 +1,7 @@
+import knex from 'knex';
+const configs = require('../knexfile.js');
+const environment: string = process.env.NODE_ENV || 'development';
+
+const db = knex(configs[environment]);
+
+module.exports = db;
--- knexfile.ts
+++ knexfile.ts
@@ -1,3 +1,5 @@
+import { Knex } from 'knex';
+
 const sharedConfig = {
   client: 'sqlite3',
   useNullAsDefault: true,
@@ -8,13 +10,13 @@ const sharedConfig = {
     directory: './data/seeds',
   },
   pool: {
-    afterCreate: (conn, done) => {
+    afterCreate: (conn: any, done: any) => {
       conn.run('PRAGMA foreign_keys = ON', done)
     },
   },
 }
 
-module.exports = {
+const configs: { [key: string]: Knex.Config } = {
   development: {
     ...sharedConfig,
     connection: { filename: './data/lambda.db3' },
@@ -24,3 +26,7 @@ module.exports = {
     connection: { filename: './data/testing.db3' },
   },
 }
+
+// cannot do export statement with knex, must be CommonJS module.exports
+// see https://stackoverflow.com/questions/52093618/knex-required-configuration-option-client-is-missing-error
+module.exports = configs;
