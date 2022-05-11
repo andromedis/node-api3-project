96cad87a7924623ec2861af90e51ac65886b1fa6
Rebecca Peckman <rebeccapeckman@gmail.com>
2022-05-10 17:45:41 -0700

    cleanup seed refactored, run

--- data/seeds/000-cleanup.ts
+++ data/seeds/000-cleanup.ts
@@ -1,6 +1,7 @@
-const knexCleaner = require("knex-cleaner");
+import { Knex } from 'knex';
+import knexCleaner from 'knex-cleaner';
 
-exports.seed = function(knex) {
+exports.seed = function(knex: Knex) {
   return knexCleaner.clean(knex, {
     ignoreTables: ["knex_migrations", "knex_migrations_lock"]
   });
